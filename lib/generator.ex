42dea25cf6b5ae0c62d1ca32ff5ec73819066c35
Grant Zukowski <grant.zukowski@nfiindustries.com>
2021-07-15 22:19:56 -0500

    working basically, need a little more validation

--- lib/generator.ex
+++ lib/generator.ex
@@ -7,6 +7,10 @@ defmodule Generator do
   if it comes from the database, an external API or others.
   """
 
+  @letters (Enum.to_list(?a..?z) ++ Enum.to_list(?A..?Z)) |> Enum.map(fn n -> <<n>> end)
+  @numbers Enum.to_list(?0..?9) |> Enum.map(fn n -> <<n>> end)
+  @special_characters Enum.to_list(?!..?.) |> Enum.map(fn n -> <<n>> end)
+
   def include_password(params) do
     %{
       "length" => params["length"],
@@ -18,6 +22,27 @@ defmodule Generator do
   end
 
   defp generate_password(length, numbers?, special_characters?) do
-    "changeit"
+    # Stream.repeatedly(fn -> Enum.random(generate_value_list(numbers?, special_characters?)) end)
+    # |> IO.inspect()
+    # |> Enum.take(length)
+
+    # |> Enum.join()
+    # |> IO.inspect()
+    {integer_length, ""} = Integer.parse(length)
+
+    Stream.repeatedly(fn -> Enum.random(generate_value_list(numbers?, special_characters?)) end)
+    |> Enum.take(integer_length)
+    |> Enum.join()
   end
+
+  defp generate_value_list(numbers?, special_characters?)
+
+  defp generate_value_list("true", "true") do
+    (@letters ++ @numbers ++ @special_characters) |> IO.inspect()
+  end
+
+  defp generate_value_list("true", "false"), do: @letters ++ @numbers
+  defp generate_value_list("false", "true"), do: @letters ++ @special_characters
+
+  defp generate_value_list("false", "false"), do: @letters
 end
